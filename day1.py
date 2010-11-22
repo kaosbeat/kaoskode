4018fb6df428436b823bd96a770862747ba5a924
kaosbeat <kasper.jordaens@gmail.com>
2010-11-22 21:58:10 +0100

    grid and cars running around day1.2

--- day1.py
+++ day1.py
@@ -21,8 +21,16 @@ background(colors.black())
 stroke(colors.white())
 #I heard there's some good contrast between those colors
 
-#lets do rectangles
-for l in range (HEIGHT/20+1):
+#it looks like a city grid lets make streets
+for l in range (HEIGHT/20):
+    #and cars
+    for c in range(random(60)):
+        car = rect(random(400),l*20,3,1)
+        car.stroke = color(1,0,0)
     for i in range(WIDTH/20):
-        rect(i*20,(l*20)-20,18,18)
+        rect(i*20+3,(l*20+3),14,14)
+        car = rect(i*20,random(400),1,3)
+        car.stroke = color(1,0,0)
+
+
 
