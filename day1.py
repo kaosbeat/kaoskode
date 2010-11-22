3fbcd8c842b406dd60f2941219f31742db9de14a
kaosbeat <kasper.jordaens@gmail.com>
2010-11-22 21:01:28 +0100

    set some goals, make that alpha goals

---
+++ day1.py
@@ -0,0 +1,13 @@
+'''
+so the basic idea is:
+    - I write some code, 
+    - It generates some gfx, 
+    - I push the code to github 
+    - the same script makes a blogpost with the output
+    - and changes the profilepic of @kaoskode with link to the blog 
+    
+but we're not there yet
+
+on the way I will learn python, git and explore other stuff
+odds are I won't even finish todays session
+'''
