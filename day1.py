7234eff29d440a0a91a2e78f7a329881f06c2f75
kaosbeat <kasper.jordaens@gmail.com>
2010-11-23 08:28:34 +0100

    not so central park added day1.3

--- day1.py
+++ day1.py
@@ -19,6 +19,7 @@ colors = ximport("colors")
 background(colors.black())
 # we want the background black like the night from which beaty will rise (it's winter and it's late)
 stroke(colors.white())
+fill(colors.grey())
 #I heard there's some good contrast between those colors
 
 #it looks like a city grid lets make streets
@@ -26,11 +27,14 @@ for l in range (HEIGHT/20):
     #and cars
     for c in range(random(60)):
         car = rect(random(400),l*20,3,1)
-        car.stroke = color(1,0,0)
+        car.stroke = color(random(1.0),random(1.0),random(0))
     for i in range(WIDTH/20):
         rect(i*20+3,(l*20+3),14,14)
         car = rect(i*20,random(400),1,3)
-        car.stroke = color(1,0,0)
+        car.stroke = color(random(1.0),random(1.0),random(1.0))
 
 
+#add a park
 
+park = rect(random(18)*20, random(18)*20, 80,60)
+park.fill = colors.green()
